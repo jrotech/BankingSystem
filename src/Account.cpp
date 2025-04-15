92fd8aff408f5711be4e17d6d5ca7ad848a1ba10
Robert Oros <jacob.oros@hotmail.com>
2025-04-15 23:51:37 +0100

    Added Account Class

---
+++ src/Account.cpp
@@ -0,0 +1,52 @@
+#include "Account.hpp"
+#include <iostream>
+
+//Constructor
+Account::Account(const std::string &accNum, const std::string &ownNm, double initialBalance)
+    : accountNumber(accNum), ownerName(ownNm), balance(initialBalance) {}
+
+//Deposit
+void Account::deposit(double amount){
+    if (amount >= 0){
+        balance += amount;
+        std::cout<<"You deposited $" << amount << ". Your balance is now: $" << balance <<"\n";
+    }
+    else
+        std::cout<<"You cannot deposit a negative account\n";
+}
+
+//Withdraw
+void Account::withdraw(double amount){
+    if (amount <= balance){
+        balance -=  amount;
+        std::cout<<"You withdrew $" << amount << ". Your balance is now: $" << balance <<"\n";
+    }
+    else
+        std::cout<<"You cannot withdraw more than your current balance: $ " << balance <<"\n";
+}
+
+//Display Details
+void Account::displayDetails() const{
+    std::cout<< "Account Number: " << accountNumber << "\n" 
+             << "Owner: " << ownerName << "\n"
+             << "Balance: " << balance << "\n";
+}
+
+//Getters
+double Account::getBalance() const{
+    return balance;
+}
+std::string Account::getOwnerName() const{
+    return ownerName;
+}
+std::string Account::getAccountNumber() const{
+    return accountNumber;
+}
+
+//Setters
+void Account::setAccountNumber(const std::string &number){
+    accountNumber = number;
+}
+void Account::setOwnerName(const std::string &name){
+    ownerName = name;
+}
