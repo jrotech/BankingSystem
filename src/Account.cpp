7b1014f964cb84fe4301274fc0d664beba7a14ee
Robert Oros <jacob.oros@hotmail.com>
2025-04-16 18:43:57 +0100

    Integrated Deposit Transaction and Withdraw Transaction in Account

--- src/Account.cpp
+++ src/Account.cpp
@@ -1,15 +1,26 @@
 #include "Account.hpp"
 #include <iostream>
+#include <memory>
+#include "DepositTransaction.hpp"
+#include "WithdrawTransaction.hpp"
+#include "TransferTransaction.hpp"
 
 //Constructor
 Account::Account(const std::string &accNum, const std::string &ownNm, double initialBalance)
-    : accountNumber(accNum), ownerName(ownNm), balance(initialBalance) {}
+    : accountNumber(accNum), ownerName(ownNm), balance(initialBalance), {}
+
+//Add Transaction
+void Account::addTransaction(std::shared_ptr<Transaction> transaction){
+    transactions.push_back(transaction);
+}
 
 //Deposit
 void Account::deposit(double amount){
     if (amount >= 0){
         balance += amount;
         std::cout<<"You deposited $" << amount << ". Your balance is now: $" << balance <<"\n";
+        std::shared_ptr<DepositTransaction> tx =  std::make_shared<DepositTransaction>(amount,accountNumber);
+        addTransaction(tx);
     }
     else
         std::cout<<"You cannot deposit a negative account\n";
@@ -20,6 +31,8 @@ void Account::withdraw(double amount){
     if (amount <= balance){
         balance -=  amount;
         std::cout<<"You withdrew $" << amount << ". Your balance is now: $" << balance <<"\n";
+        std::shared_ptr<WithdrawTransaction> tx =  std::make_shared<WithdrawTransaction>(amount,accountNumber);
+        addTransaction(tx);
     }
     else
         std::cout<<"You cannot withdraw more than your current balance: $ " << balance <<"\n";
