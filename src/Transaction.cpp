657bd7421b5282d5c6d1f00c693abb6e59bfc41c
Robert Oros <jacob.oros@hotmail.com>
2025-04-16 10:51:28 +0100

    Redesigned Transactin Constructor

--- src/Transaction.cpp
+++ src/Transaction.cpp
@@ -5,12 +5,8 @@
 int Transaction::nextID = 1;
 
 //Constructor
-Transaction::Transaction(double amt, const std::string &accNum) : amount(amt), accountNumber(accNum) {
-    transactionID = nextID;
-    nextID++;
-    timestamp = std::time(nullptr);
+Transaction::Transaction(double amt, const std::string &accNum) : amount(amt), accountNumber(accNum), transactionID(nextID++), timestamp(std::time(nullptr)){
     std::cout << "Transaction " << transactionID << " created for account " << accountNumber << "\n";
-
 }
 
 //getType and display are virtual
