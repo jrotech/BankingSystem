4489b8de3cba91a4aadeca7c1c467530f2de0ea8
Robert Oros <jacob.oros@hotmail.com>
2025-04-19 22:24:01 +0100

    Added Interest to Account Header

--- include/Account.hpp
+++ include/Account.hpp
@@ -10,16 +10,19 @@ private:
     std::string accountNumber;
     std::string ownerName;
     double balance;
+    double interest;
     std::vector<std::shared_ptr<Transaction>> transactions;
 
 public:
-    Account(const std::string &accNum, const std::string &ownNm, double initialBalance);
+    Account(const std::string &accNum, const std::string &ownNm, double initialBalance, double rate=0.025);
     ~Account() = default;
 
     void deposit(double amount);
-    void withdraw(double amount);
+    void virtual withdraw(double amount);
+    void virtual applyInterest();
     void transfer(const std::shared_ptr<Bank> &bank, double amount, const std::string &toAccountNumber);
     void displayDetails() const;
+    
     double getBalance() const;
     std::string getOwnerName() const;
     std::string getAccountNumber() const;
